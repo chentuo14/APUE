e1fc90506dfc37d467753fbcba34b9bdf300a761
Chen Tuo <ch122633@126.com>
2017-12-26 04:40:24 -0800

    ptym_open ptys_open

---
+++ chapter_19/19-9.c
@@ -0,0 +1,57 @@
+#include "apue.h"
+#include <errno.h>
+#include <fcntl.h>
+#if defined(SOLARIS)
+#include <stropts.h>
+#endif
+
+int ptym_open(char *pts_name, int pts_namesz)
+{
+	char *ptr;
+	int fdm, err;
+
+	if((fdm = posix_openpt(O_RDWR)) < 0)
+		return(-1);
+	if(grantpt(fdm) < 0)
+		goto errout;
+	if(unlock(fdm) < 0)
+		goto errout;
+	if((ptr = ptsname(fmd)) == NULL)
+		goto errout;
+
+	strncpy(pts_name, ptr, pts_namesz);
+	pts_name[pts_namesz - 1] = '\0';
+	return(fdm);
+errout:
+	err = errno;
+	close(fdm);
+	errno = err;
+	return(-1);
+}
+
+int ptys_open(char *pts_name)
+{
+	int fds;
+#if defined(SOLARIS)
+	int err, setup;
+#endif
+
+	if((fds = open(pts_name, O_RDWR)) < 0)
+		goto errout; 
+
+	if(setup == 0) {
+		if(ioctl(fds, I_PUSH, "ptem") < 0)
+			goto errout;
+		if(ioctl(fds, I_PUSH, "ldterm") < 0)
+			goto errout;
+		if(ioctl(fds, I_PUSH, "ttcompat") < 0) {
+errout:
+			err = errno;
+			close(fds);
+			errno = err;
+			return(-1);
+		}
+	}
+#endif
+	return(fds);
+}
